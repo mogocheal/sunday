396c31dc084a80436833ca436aef60d33c1e56ba
xiajinming <46959838@qq.com>
2018-03-08 00:59:24 +0800

    first

---
+++ src/components/Weather.vue
@@ -0,0 +1,57 @@
+<template>
+  <div><span class="weather">天气：{{weather}}   </span>
+    <span @click="freshWeather">最新天气</span>
+    <p>321312</p>
+    <p>{{zxc}}</p>
+  </div>
+
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+
+  export default {
+    name: 'Weather',
+    props: {
+      weather: {
+        type: String,
+        required: false,
+        default: '默认天气'
+      }
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      freshWeather: function () {
+        this.$store.commit({
+          type: 'getWeather',
+          val: '修改后的天气'
+        })
+      }
+    },
+    computed: {
+      // Map store/user state
+      ...mapState({
+        zxc: state => {
+          console.log(state);
+         return state.index.weather
+        },
+
+      })
+    },
+//    created() {
+//      this.freshWeather()
+//    },
+
+  }
+
+
+</script>
+
+<style scoped>
+  .weather {
+    color: red;
+    margin-right: 50px;
+  }
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>欢迎来到你的vue项目</h1>
+    <Weather></Weather>
+
+  </div>
+</template>
+
+<script>
+import Weather from '../components/Weather'
+    export default {
+       components:{Weather},
+       name: 'index',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style></style>
